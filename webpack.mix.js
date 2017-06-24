854f5ad2e2cba6de435fb7a766e6e05871461739
FalkSturmfels <fritzachim2@gmail.com>
2017-06-24 16:36:33 +0200

    fixed webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,8 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css/app.css');
+    .sass('resources/assets/sass/app.scss', 'public/css/app.css')
+    .sass('resources/assets/sass/bootswatch/bootswatch.sass', "public/css");
 
 
 mix.styles([

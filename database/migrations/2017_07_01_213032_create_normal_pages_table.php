41f1ec72f90c127ae16fbb1fb15687f4a6e9178a
FalkSturmfels <fritzachim2@gmail.com>
2017-07-10 19:52:32 +0200

    Added first create table migrations for pages, menu and links

--- database/migrations/2017_07_01_213032_create_normal_pages_table.php
+++ database/migrations/2017_07_01_213032_create_normal_pages_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreatePasswordResetsTable extends Migration
+class CreateNormalPagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,10 +12,11 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('normal_pages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('body');
+            $table->timestamps();
         });
     }
 
@@ -26,6 +27,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('normal_pages');
     }
 }
--- database/migrations/2017_07_01_213121_create_diary_pages_table.php
+++ database/migrations/2017_07_01_213121_create_diary_pages_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateUsersTable extends Migration
+class CreateDiaryPagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('diary_pages', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->text('body');
+            $table->unsignedInteger('menu_link_list_id');
             $table->timestamps();
+
+
         });
     }
 
@@ -29,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('diary_pages');
     }
 }

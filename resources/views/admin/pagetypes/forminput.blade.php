b785045c674ab650fe060d6c88de9e31d2810593
FalkSturmfels <fritzachim2@gmail.com>
2017-07-19 22:08:48 +0200

    Made page type form inputs smaller

--- resources/views/admin/pagetypes/forminput.blade.php
+++ resources/views/admin/pagetypes/forminput.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group">
     <label for="name" class="col-sm-1 control-label">Anzeigename* </label>
-    <div class="col-sm-10">
+    <div class="col-sm-3">
         <input type="text" class="form-control" id="displayName" name="displayName"
                placeholder="Anzeigename"
                 @include('partials.forminputs.oldorentityvalue', ["type" => "displayName"])>
@@ -9,7 +9,7 @@
 
 <div class="form-group">
     <label for="name" class="col-sm-1 control-label">Typ* </label>
-    <div class="col-sm-10">
+    <div class="col-sm-3">
         <input type="text" class="form-control" id="type" name="type"
                placeholder="Typ"
                 @include('partials.forminputs.oldorentityvalue', ["type" => "type"])>
